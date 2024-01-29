0dc7419ccb8ecbf38fa92dcb12ca2051f2666d0c
Can BELLİBAŞLILAR <bellibasllarcn@gmail.com>
2024-01-29 09:49:28 +0300

    .

--- lib/constants/theme/app_colors.dart
+++ lib/constants/theme/app_colors.dart
@@ -4,10 +4,6 @@ abstract class AppColor {
   static Color bgColor = const Color(0xFFFFFFFF);
   static Color black = const Color(0xFF000000);
   static Color textColor = const Color(0xFF000000);
-  static Color primaryColor = const Color(0xFF0F172A);
-  static Color normalTextColor = const Color(0xFF64748B);
-  static Color articleCardBG = const Color(0xFFD9D9D9);
-  static Color linkColor = const Color(0xFF2563EB);
   static Color errorColor = const Color(0xFF7A0619);
   static Color successColor = const Color(0xFF5cb85c);
 }
