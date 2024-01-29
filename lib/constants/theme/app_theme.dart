6eb6f10100c0a67fa8282f1570d78f8ee32fe50f
Can BELLİBAŞLILAR <bellibasllarcn@gmail.com>
2024-01-29 09:50:44 +0300

    .

--- lib/constants/theme/app_theme.dart
+++ lib/constants/theme/app_theme.dart
@@ -1,6 +1,5 @@
 import 'package:beymen_testcase/constants/theme/app_colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 ThemeData theme() {
@@ -8,7 +7,6 @@ ThemeData theme() {
     scaffoldBackgroundColor: AppColor.bgColor,
     textTheme: GoogleFonts.robotoTextTheme(),
     floatingActionButtonTheme: floatingActionButtonThemeData,
-    // appBarTheme: appBarTheme(),
   );
 }
 
@@ -16,17 +14,3 @@ FloatingActionButtonThemeData floatingActionButtonThemeData =
     FloatingActionButtonThemeData(
   backgroundColor: AppColor.bgColor,
 );
-
-AppBarTheme appBarTheme() {
-  return const AppBarTheme(
-    systemOverlayStyle: SystemUiOverlayStyle(
-      systemNavigationBarColor: Colors.blue, // Navigation bar
-      statusBarColor: Colors.pink, // Status bar
-    ),
-    color: Colors.white,
-    elevation: 0,
-    centerTitle: true,
-    iconTheme: IconThemeData(color: Color(0XFF8B8B8B)),
-    titleTextStyle: TextStyle(color: Color(0XFF8B8B8B), fontSize: 18),
-  );
-}
